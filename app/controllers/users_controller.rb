6b314a49f68d4ea071990dc349ca87414b382238
tony un <tony un>
2016-08-04 17:26:57 -0400

    can create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
 
-  def new 
-    #code
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user = User.create(user_params)
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
