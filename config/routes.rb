79e3fc8b887bd62c8fa8d7ac7b3d49cf3fbe598a
tony un <tony un>
2016-08-05 11:52:08 -0400

    adding a sessions controller, view, and resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#index"
   resources :users, only: [:new, :create]
-  
+  resources :sessions, only: [:new, :create, :delete]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
