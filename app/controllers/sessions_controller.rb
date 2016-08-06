374f02a91c39347c51f8a4dd51b8f25535c8f91b
tony un <tony un>
2016-08-06 16:11:21 -0400

    adding a tweets controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
+
   def new
-  
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Cheers"
+    else
+      redirect_to root_path, notice: "You failed"
+    end
+  end
+
+  def delete
+    
   end
 end
