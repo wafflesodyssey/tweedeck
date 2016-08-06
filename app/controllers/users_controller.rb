ac98856d8f1cec898382e5cba561f8f9038a5fb6
tony un <tony un>
2016-08-06 16:33:03 -0400

    adding the method def show in the Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     if @user = User.create(user_params)
-      redirect_to root_path
+        redirect_to root_path
     else
-      redirect_to new_user_path
+        redirect_to new_user_path
+    end
+
+    def show
     end
   end
+
 
   private def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
